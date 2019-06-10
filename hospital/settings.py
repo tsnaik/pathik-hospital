b2ca112be3da45510f9ee5f25b1ada4bdd00f999
tsnaik <tnaik96@gmail.com>
2019-06-10 18:53:38 +0530

    - Added "Patient" model

--- hospital/settings.py
+++ hospital/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp.apps.MainappConfig'
 ]
 
 MIDDLEWARE = [
@@ -73,13 +74,24 @@ WSGI_APPLICATION = 'hospital.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pathikhospital',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+#}
 
 
 # Password validation
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    #name
+    first_name = models.CharField(max_length=50, db_index=True)
+    middle_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    #address
+    address1 = models.CharField(max_length=1000)
+    address2 = models.CharField(max_length=1000, blank=True, null=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=15, default='India')
+    zip_code = models.CharField(max_length=15, blank=True, null=True)
+
+    dob = models.DateField()
+    age = models.PositiveSmallIntegerField()
+
+    #sex
+    M = 'M'
+    F = 'F'
+    O = 'O'
+    SEX_CHOICES = [
+        (M, 'Male'),
+        (F, 'Female'),
+        (O, 'Other'),
+    ]
+    sex = models.CharField(
+        max_length=2,
+        choices=SEX_CHOICES,
+        default=M,
+    )
+
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField(max_length=50, blank=True, null=True)
+
+    blood_group = models.CharField(max_length=5, blank=True, null=True)
+    remarks = models.TextField()
+
+    date_of_registration = models.DateTimeField(auto_now_add=True)
+
+    #TODO add file column
