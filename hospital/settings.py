dfc902d407c635134145636b10d80c5d2570667a
tsnaik <tnaik96@gmail.com>
2019-06-10 18:58:40 +0530

    Removed sqlite

--- hospital/settings.py
+++ hospital/settings.py
@@ -86,13 +86,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-#}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
