065b8ea71b3c3d7003f1e0b8a98666ff15fa3c29
tsnaik <tnaik96@gmail.com>
2019-06-13 18:22:47 +0530

    Added 'Manage Patients' webpage with non-functioning patient registration form

---
+++ mainapp/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from mainapp.models import Patient
+
+
+class RegForm(forms.ModelForm):
+    class Meta:
+        model = Patient
+        fields = ['first_name', 'middle_name', 'last_name', 'address1', 'address2', 'city', 'state',
+                  'country', 'zip_code', 'dob', 'sex', 'phone_number', 'email', 'blood_group', 'remarks']
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,24 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+from mainapp.forms import RegForm
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = RegForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = RegForm()
+
+    return render(request, 'mainapp/index.html', {'form': form})
